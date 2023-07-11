a0b044aeecb109189b60fe554f788ee510a7df1f
danielpereira91 <daniel_t.pereira@hotmail.com>
2023-07-11 01:14:40 -0300

    Aula 11 - Tipos primitivos pt 2 - Adicionando outros tipos primitivos, utilizando Unicode e tabela ASCII

--- src/academy/devdojo/maratonajava/introducao/TiposPrimitivos.java
+++ src/academy/devdojo/maratonajava/introducao/TiposPrimitivos.java
@@ -4,7 +4,23 @@ public class TiposPrimitivos {
     public static void main(String[] args) {
         // int, double, float, char, byte, short, long, boolean
         int age = 10;
+        double salaryDouble = 2000;
+        float salaryFloat = 2500;
+        byte ageByte = 10;
+        short ageShort = 10;
+        boolean truth = true;
+        boolean fake = false;
+        String word; // Era para ser "char" porém o IntelliJ fica forçando mudar para String
+        word = "C";
+        char asciiTable = 68;
+        char uniCode = '\u004C';
+
         System.out.println(age);
         System.out.println("A idade é "+age+" anos.");
+        System.out.println(truth);
+        System.out.println(fake);
+        System.out.println(uniCode + "uísa");
+        System.out.println(word + "amila");
+        System.out.println(asciiTable + "aniel");
     }
 }
