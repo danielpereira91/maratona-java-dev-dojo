012b9b304054176f697cda2cacbc1b93841a2e13
danielpereira91 <daniel_t.pereira@hotmail.com>
2023-07-11 00:35:28 -0300

    Aula 09 - Adicionando alguns comentários e javadoc

--- src/academy/devdojo/maratonajava/introducao/OlaDevDojo.java
+++ src/academy/devdojo/maratonajava/introducao/OlaDevDojo.java
@@ -1,7 +1,16 @@
 package academy.devdojo.maratonajava.introducao;
 
+/**
+ * Comentário <b>javadoc</b>
+ */
 public class OlaDevDojo {
+    //Código de introdução do curso
+    /*
+    Ao invés de ser "olá, mundo",
+    é Goku!
+     */
         public static void main(String[] args){
+            // Imprime algo <- Comentário bobo, desnecessário e suja o código (Clean Code)
             System.out.println("KA-ME-HA-ME-HA");
         }
 }
