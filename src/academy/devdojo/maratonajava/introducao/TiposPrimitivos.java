5d26f3f2d845534ffe552d9ff3067f43ce18e551
danielpereira91 <daniel_t.pereira@hotmail.com>
2023-07-11 00:51:24 -0300

    Aula 10 - Tipos primitivos pt 1 - Inserindo uma variável int e imprimindo

---
+++ src/academy/devdojo/maratonajava/introducao/TiposPrimitivos.java
@@ -0,0 +1,10 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class TiposPrimitivos {
+    public static void main(String[] args) {
+        // int, double, float, char, byte, short, long, boolean
+        int age = 10;
+        System.out.println(age);
+        System.out.println("A idade é "+age+" anos.");
+    }
+}
