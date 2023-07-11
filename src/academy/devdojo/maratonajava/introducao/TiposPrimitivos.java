7768838cd26cb06196bf73ed17ef4036fa6d7aee
danielpereira91 <daniel_t.pereira@hotmail.com>
2023-07-11 01:37:43 -0300

    Aula 12- Tipos primitivos pt 3 - utilizando casting e letra no final do valor para reafirmar float

--- src/academy/devdojo/maratonajava/introducao/TiposPrimitivos.java
+++ src/academy/devdojo/maratonajava/introducao/TiposPrimitivos.java
@@ -4,13 +4,15 @@ public class TiposPrimitivos {
     public static void main(String[] args) {
         // int, double, float, char, byte, short, long, boolean
         int age = 10;
-        double salaryDouble = 2000;
-        float salaryFloat = 2500;
+        double salaryInt = (int) 3457.89; // Casting - não vai aparecer o decimal pois foi dado Casting para transformar o valor em inteiro
+        long universeAge = 13800000000l; // "l" no final para especificar que é um inteiro grande.
+        double salaryDouble = 2000.0; // Aceita o ponto sem precisar colocar o "d" no final, mas pode colocar também
+        float salaryFloat = 2500.0f; // Se não colocar o "f" no final, o Java solicita que seja mudado para Double
         byte ageByte = 10;
         short ageShort = 10;
         boolean truth = true;
         boolean fake = false;
-        String word; // Era para ser "char" porém o IntelliJ fica forçando mudar para String
+        String word; // Era para ser "char" porém o Java fica forçando mudar para String
         word = "C";
         char asciiTable = 68;
         char uniCode = '\u004C';
@@ -22,5 +24,7 @@ public class TiposPrimitivos {
         System.out.println(uniCode + "uísa");
         System.out.println(word + "amila");
         System.out.println(asciiTable + "aniel");
+        System.out.println("Age of the Universe: " + universeAge);
+        System.out.println(salaryInt);
     }
 }
