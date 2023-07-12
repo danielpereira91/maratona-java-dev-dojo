d94066ea824fa1ca72fd9964918c36799bcf0bea
danielpereira91 <daniel_t.pereira@hotmail.com>
2023-07-12 10:41:59 -0300

    Aula 20 - Estruturas Condicionais pt 1 - If

---
+++ src/academy/devdojo/maratonajava/introducao/EstruturasCondicionais.java
@@ -0,0 +1,23 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class EstruturasCondicionais {
+    public static void main(String[] args) {
+        int age = 15;
+        boolean isAutorizadoComprarBebidaAlcolica = age >=18;
+
+        if (isAutorizadoComprarBebidaAlcolica) {
+            System.out.println("Autorizado a comprar bebida alcólica");
+        }
+
+        // !
+        if (!isAutorizadoComprarBebidaAlcolica) {
+            System.out.println("Não Autorizado a comprar bebida alcólica.");
+        }
+
+        boolean c = false;
+        if (c = true) { // Está sendo atribuído o valor True a c. Para ser comparado, deveria ser "==".
+            System.out.println("Dentro de algo que nunca deve ser feito.");
+        }
+        System.out.println("Fora do if");
+    }
+}
