db87ab9841437bff71483d5223e0e22bfc99c107
danielpereira91 <daniel_t.pereira@hotmail.com>
2023-07-12 15:18:56 -0300

    Adicionando '01' no nome do arquivo EstruturasCondicionais

--- src/academy/devdojo/maratonajava/introducao/EstruturasCondicionais01.java
+++ src/academy/devdojo/maratonajava/introducao/EstruturasCondicionais01.java
@@ -1,6 +1,6 @@
 package academy.devdojo.maratonajava.introducao;
 
-public class EstruturasCondicionais {
+public class EstruturasCondicionais01 {
     public static void main(String[] args) {
         int age = 15;
         boolean isAutorizadoComprarBebidaAlcolica = age >= 18;
