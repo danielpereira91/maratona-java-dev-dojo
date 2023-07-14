3d3a5cd15584987e13b4972630ffc9039c24b223
danielpereira91 <daniel_t.pereira@hotmail.com>
2023-07-14 01:16:10 -0300

    Aula 30 - Exercício com Break em Estruturas de Repetição

---
+++ src/academy/devdojo/maratonajava/introducao/EstruturasDeRepeticaoExercicioBreak.java
@@ -0,0 +1,18 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class EstruturasDeRepeticaoExercicioBreak {
+    public static void main(String[] args) {
+        // Dado o valor de um carro, descubra em quantas vezes ele pode ser parcelado.
+        // valorParcela >= 1000
+        double valorCarro = 62000;
+        for (int parcela = 1; parcela <= valorCarro ; parcela++) {
+            double valorParcela = valorCarro / parcela;
+            if (valorParcela < 1000) {
+                break;
+            }
+                System.out.printf("Parcela %d: R$%.2f.", parcela, valorParcela);
+                System.out.println("");
+
+        }
+    }
+}
