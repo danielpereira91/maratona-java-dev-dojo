b4be5d8eb52fefd116b322559cc9f1b426af206a
danielpereira91 <daniel_t.pereira@hotmail.com>
2023-07-15 05:45:05 -0300

    Aula 35 - Arrays pt 04 - Foreach

---
+++ src/academy/devdojo/maratonajava/introducao/Arrays03.java
@@ -0,0 +1,19 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class Arrays03 {
+    public static void main(String[] args) {
+        int[] numeros = new int[3];
+        int[] numeros2 = {1, 2, 3, 4, 5};
+        int[] numeros3 = new int[]{5, 4, 3, 2, 1}; // mesmo resultado de numero2
+
+        for (int i = 0; i < numeros2.length; i++) {
+            System.out.println(numeros2[i]);
+        }
+
+        System.out.println("");
+
+        for (int num : numeros3) {
+            System.out.println(num);
+        }
+    }
+}
