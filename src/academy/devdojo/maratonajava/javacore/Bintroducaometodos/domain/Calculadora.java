4f9771c349ac190e812496e3a777745c1a8b9143
danielpereira91 <daniel_t.pereira@hotmail.com>
2023-07-18 01:03:31 -0300

    Aula 47 - Orientação Objetos - Métodos pt 04 - Retorno pt 02

--- src/academy/devdojo/maratonajava/javacore/Bintroducaometodos/domain/Calculadora.java
+++ src/academy/devdojo/maratonajava/javacore/Bintroducaometodos/domain/Calculadora.java
@@ -20,4 +20,20 @@ public class Calculadora {
         }
         return num1 / num2;
     }
+
+    public double divideDoisNumeros02(double num1, double num2) {
+        if (num2 == 0) {
+            return 0;
+        } else {
+            return num1 / num2;
+        }
+    }
+
+    public void imprimeDivisaoDeDoisNumeros(double num1, double num2) {
+        if (num2 == 0) {
+            System.out.println("Não existe divisão por Zero");
+            return;
+        }
+        System.out.println(num1 / num2);
+    }
 }
--- src/academy/devdojo/maratonajava/javacore/Bintroducaometodos/test/CalculadoraTest03.java
+++ src/academy/devdojo/maratonajava/javacore/Bintroducaometodos/test/CalculadoraTest03.java
@@ -6,7 +6,11 @@ public class CalculadoraTest03 {
     public static void main(String[] args) {
         Calculadora calculadora = new Calculadora();
         double resultado = calculadora.divideDoisNumeros(20, 0);
+
         System.out.println(resultado);
         System.out.println(calculadora.divideDoisNumeros(20, 2));
+        System.out.println("----------------");
+        calculadora.imprimeDivisaoDeDoisNumeros(86, 5);
+        calculadora.imprimeDivisaoDeDoisNumeros(86, 0);
     }
 }
