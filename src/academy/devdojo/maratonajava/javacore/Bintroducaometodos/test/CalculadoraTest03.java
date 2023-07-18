ab7fe7643d6366da1d6adb7e7da6f516daa14ea3
danielpereira91 <daniel_t.pereira@hotmail.com>
2023-07-18 00:54:20 -0300

    Aula 46 - Orientação Objetos - Métodos pt 03 - Retorno pt 01

---
+++ src/academy/devdojo/maratonajava/javacore/Bintroducaometodos/test/CalculadoraTest03.java
@@ -0,0 +1,12 @@
+package academy.devdojo.maratonajava.javacore.Bintroducaometodos.test;
+
+import academy.devdojo.maratonajava.javacore.Bintroducaometodos.domain.Calculadora;
+
+public class CalculadoraTest03 {
+    public static void main(String[] args) {
+        Calculadora calculadora = new Calculadora();
+        double resultado = calculadora.divideDoisNumeros(20, 0);
+        System.out.println(resultado);
+        System.out.println(calculadora.divideDoisNumeros(20, 2));
+    }
+}
