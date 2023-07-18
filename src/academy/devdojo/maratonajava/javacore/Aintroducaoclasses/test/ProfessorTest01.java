7566f045801c689037ebca8f8352bfe0b80158e3
danielpereira91 <daniel_t.pereira@hotmail.com>
2023-07-17 23:53:04 -0300

    Aula 41 - Orientação Objetos - Coesão

---
+++ src/academy/devdojo/maratonajava/javacore/Aintroducaoclasses/test/ProfessorTest01.java
@@ -0,0 +1,16 @@
+package academy.devdojo.maratonajava.javacore.Aintroducaoclasses.test;
+
+import academy.devdojo.maratonajava.javacore.Aintroducaoclasses.domain.Professor;
+
+public class ProfessorTest01 {
+    public static void main(String[] args) {
+        Professor professor = new Professor();
+        professor.nome = "Mestre Kami";
+        professor.idade = 140;
+        professor.sexo = 'M';
+
+        System.out.print("Nome: " + professor.nome);
+        System.out.print(" Idade: " + professor.idade);
+        System.out.print(" Sexo: " + professor.sexo);
+    }
+}
