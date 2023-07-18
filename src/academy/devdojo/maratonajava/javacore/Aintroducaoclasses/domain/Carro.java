adede69d6bc7eb24625a9893af13d8870e9408fc
danielpereira91 <daniel_t.pereira@hotmail.com>
2023-07-18 00:14:14 -0300

    Tirei o termo 'Exercicio' das classes Carro e CarroTest

--- src/academy/devdojo/maratonajava/javacore/Aintroducaoclasses/domain/Carro.java
+++ src/academy/devdojo/maratonajava/javacore/Aintroducaoclasses/domain/Carro.java
@@ -1,6 +1,6 @@
 package academy.devdojo.maratonajava.javacore.Aintroducaoclasses.domain;
 
-public class CarroExercicio {
+public class Carro {
     public String nome;
     public String modelo;
     public int ano;
--- src/academy/devdojo/maratonajava/javacore/Aintroducaoclasses/test/CarroTest.java
+++ src/academy/devdojo/maratonajava/javacore/Aintroducaoclasses/test/CarroTest.java
@@ -1,11 +1,11 @@
 package academy.devdojo.maratonajava.javacore.Aintroducaoclasses.test;
 
-import academy.devdojo.maratonajava.javacore.Aintroducaoclasses.domain.CarroExercicio;
+import academy.devdojo.maratonajava.javacore.Aintroducaoclasses.domain.Carro;
 
-public class CarroExercicioTest {
+public class CarroTest {
     public static void main(String[] args) {
-        CarroExercicio carro1 = new CarroExercicio();
-        CarroExercicio carro2 = new CarroExercicio();
+        Carro carro1 = new Carro();
+        Carro carro2 = new Carro();
 
         carro1.nome = "Nissan";
         carro1.modelo = "Versa";
