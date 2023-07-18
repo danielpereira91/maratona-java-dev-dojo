04cdc1dac30acb1c21244be4ba94564a27cdad20
danielpereira91 <daniel_t.pereira@hotmail.com>
2023-07-18 00:43:47 -0300

    Aula 45 - Orientação Objetos - Métodos pt 02 - Parâmetros

--- src/academy/devdojo/maratonajava/javacore/Bintroducaometodos/test/CalculadoraTest02.java
+++ src/academy/devdojo/maratonajava/javacore/Bintroducaometodos/test/CalculadoraTest02.java
@@ -2,11 +2,9 @@ package academy.devdojo.maratonajava.javacore.Bintroducaometodos.test;
 
 import academy.devdojo.maratonajava.javacore.Bintroducaometodos.domain.Calculadora;
 
-public class CalculadoraTest01 {
+public class CalculadoraTest02 {
     public static void main(String[] args) {
         Calculadora calculadora = new Calculadora();
-        calculadora.somaDoisNumeros();
-        System.out.println("Finalizanqdo CalculadoraTest01");
-        calculadora.subtraiDoisNumeros();
+        calculadora.multiplicaDoisNumeros(10, 20);
     }
 }
