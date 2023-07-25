0a29dedc242ec02c8158a797d35bde6d2401d721
danielpereira91 <daniel_t.pereira@hotmail.com>
2023-07-25 14:35:44 -0300

    Iniciando exercício da aula 53

---
+++ src/academy/devdojo/maratonajava/javacore/Bintroducaometodos/domain/Funcionario.java
@@ -0,0 +1,14 @@
+package academy.devdojo.maratonajava.javacore.Bintroducaometodos.domain;
+
+public class Funcionario {
+    public String nome;
+    public byte idade;
+    public double salarios;
+
+    public void imprimeFuncionario() {
+        System.out.println(this.nome);
+        System.out.println(this.idade);
+        System.out.println(this.);
+    }
+}
+
