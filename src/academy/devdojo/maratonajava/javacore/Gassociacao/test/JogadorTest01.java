f450856c213baf79003b505683a9b5b89c64e791
danielpereira91 <daniel_t.pereira@hotmail.com>
2023-08-09 01:25:20 -0300

    Aula 67 - Orientação Objetos - Associação pt 04 - Associação bidirecional

--- src/academy/devdojo/maratonajava/javacore/Gassociacao/test/JogadorTest01.java
+++ src/academy/devdojo/maratonajava/javacore/Gassociacao/test/JogadorTest01.java
@@ -6,7 +6,7 @@ public class JogadorTest01 {
     public static void main(String[] args) {
         Jogador jogador1 = new Jogador("Pelé");
         Jogador jogador2 = new Jogador("Romário");
-        Jogador jogador3 = new Jogador("Cafú");
+        Jogador jogador3 = new Jogador("Cafu");
         // Jogador[] jogadores = new Jogador[3]; OU
         // Jogador[] jogadores = new Jogador[]{jogador1, jogador2, jogador3}; OU
         Jogador[] jogadores = {jogador1, jogador2, jogador3};
