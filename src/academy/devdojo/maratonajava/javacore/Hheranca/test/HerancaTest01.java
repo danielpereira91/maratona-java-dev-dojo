bc08b2cfe8d38590d211821bf743204225d44789
danielpereira91 <daniel_t.pereira@hotmail.com>
2023-08-12 00:30:37 -0300

    Aula 71 - Orientação Objetos - Herança pt 01

---
+++ src/academy/devdojo/maratonajava/javacore/Hheranca/test/HerancaTest01.java
@@ -0,0 +1,29 @@
+package academy.devdojo.maratonajava.javacore.Hheranca.test;
+
+import academy.devdojo.maratonajava.javacore.Hheranca.domain.Endereco;
+import academy.devdojo.maratonajava.javacore.Hheranca.domain.Funcionario;
+import academy.devdojo.maratonajava.javacore.Hheranca.domain.Pessoa;
+
+public class HerancaTest01 {
+    public static void main(String[] args) {
+        Endereco endereco = new Endereco();
+        endereco.setCep("01234-567");
+        endereco.setRua("Rua 3700");
+
+        Pessoa pessoa = new Pessoa();
+        pessoa.setNome("Toyohisa Shimazu");
+        pessoa.setEndereco(endereco);
+        pessoa.setCpf("012.345.678-90");
+
+        pessoa.imprime();
+
+        Funcionario funcionario = new Funcionario();
+        funcionario.setNome("Oda Nobunaga");
+        funcionario.setCpf("123.456.789.10");
+        funcionario.setEndereco(endereco);
+        funcionario.setSalario(20000);
+
+        funcionario.imprime();
+        System.out.println(funcionario.getSalario());
+    }
+}
