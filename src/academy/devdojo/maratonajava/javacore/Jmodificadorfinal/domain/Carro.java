1b0f393c2add49678a05f3587f971d19b46cdd31
danielpereira91 <daniel_t.pereira@hotmail.com>
2023-08-15 00:35:54 -0300

    Aula 77 - Orientação Objetos - Modificador final pt 01 - Tipo

---
+++ src/academy/devdojo/maratonajava/javacore/Jmodificadorfinal/domain/Carro.java
@@ -0,0 +1,14 @@
+package academy.devdojo.maratonajava.javacore.Jmodificadorfinal.domain;
+
+public class Carro {
+    private String nome;
+    public static final double VELOCIDADE_LIMITE = 250; // toda variável constante (final) tem que estar em uppercase e separada por underscore.
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+}
