d18ab982948e9d433aaf75b934feb880754d46cf
danielpereira91 <daniel_t.pereira@hotmail.com>
2023-08-22 02:31:15 -0300

    Aula 82 - Orientação Objetos - Enumeração pt 03 - Sobrescrita de métodos

--- src/academy/devdojo/maratonajava/javacore/Kenum/domain/Cliente.java
+++ src/academy/devdojo/maratonajava/javacore/Kenum/domain/Cliente.java
@@ -1,10 +1,6 @@
 package academy.devdojo.maratonajava.javacore.Kenum.domain;
 
 public class Cliente {
-    public enum TipoPagamento {
-        DEBITO, CREDITO
-    }
-
     private String nome;
     private TipoCliente tipoCliente;
     private TipoPagamento tipoPagamento;
--- src/academy/devdojo/maratonajava/javacore/Kenum/test/ClienteTest01.java
+++ src/academy/devdojo/maratonajava/javacore/Kenum/test/ClienteTest01.java
@@ -1,8 +1,8 @@
 package academy.devdojo.maratonajava.javacore.Kenum.test;
 
 import academy.devdojo.maratonajava.javacore.Kenum.domain.Cliente;
-import academy.devdojo.maratonajava.javacore.Kenum.domain.Cliente.TipoPagamento;
 import academy.devdojo.maratonajava.javacore.Kenum.domain.TipoCliente;
+import academy.devdojo.maratonajava.javacore.Kenum.domain.TipoPagamento;
 
 public class ClienteTest01 {
     public static void main(String[] args) {
@@ -11,5 +11,7 @@ public class ClienteTest01 {
 
         System.out.println(cliente1);
         System.out.println(cliente2);
+        System.out.println(TipoPagamento.DEBITO.calcularDesconto(100));
+        System.out.println(TipoPagamento.CREDITO.calcularDesconto(100));
     }
 }
