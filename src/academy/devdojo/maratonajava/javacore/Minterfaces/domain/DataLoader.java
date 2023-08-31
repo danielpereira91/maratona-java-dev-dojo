338f2be53bf0a4d1b5deb42604c4fde8f4b3f974
danielpereira91 <daniel_t.pereira@hotmail.com>
2023-08-30 21:48:33 -0300

    Aula 88 - Orientação Objetos - Interfaces pt 02 - Implementando múltiplas interfaces

--- src/academy/devdojo/maratonajava/javacore/Minterfaces/domain/DataLoader.java
+++ src/academy/devdojo/maratonajava/javacore/Minterfaces/domain/DataLoader.java
@@ -2,4 +2,7 @@ package academy.devdojo.maratonajava.javacore.Minterfaces.domain;
 
 public interface DataLoader {
     void load();
+    default void checkPermission(){
+        System.out.println("Fazendo checagem de permissões.");
+    }
 }
