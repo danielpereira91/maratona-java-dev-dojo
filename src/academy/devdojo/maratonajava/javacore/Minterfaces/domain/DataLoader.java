9fb001ead50952cf63ece714826712c5009d9d5d
danielpereira91 <daniel_t.pereira@hotmail.com>
2023-08-30 23:38:26 -0300

    Aula 89 - Orientação Objetos - Interfaces pt 03 - Atributos e métodos estáticos

--- src/academy/devdojo/maratonajava/javacore/Minterfaces/domain/DataLoader.java
+++ src/academy/devdojo/maratonajava/javacore/Minterfaces/domain/DataLoader.java
@@ -1,8 +1,13 @@
 package academy.devdojo.maratonajava.javacore.Minterfaces.domain;
 
 public interface DataLoader {
+    public static final int MAX_DATE_SIZE = 10;
     void load();
     default void checkPermission(){
         System.out.println("Fazendo checagem de permissões.");
+    }
+
+    public static void retrieveMaxDataSize() {
+        System.out.println("Dentro do retrieveMaxDataSize na interface DataLoader.");
     }
 }
