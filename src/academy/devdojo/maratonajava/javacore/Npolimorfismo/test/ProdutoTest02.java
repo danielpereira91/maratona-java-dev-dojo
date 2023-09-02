324cc97a81aa58c61a5e1c7d3bb18997627798fb
danielpereira91 <daniel_t.pereira@hotmail.com>
2023-09-02 01:31:57 -0300

    Aula 91 - Polimorfismo pt 02 - Funcionamento

---
+++ src/academy/devdojo/maratonajava/javacore/Npolimorfismo/test/ProdutoTest02.java
@@ -0,0 +1,21 @@
+package academy.devdojo.maratonajava.javacore.Npolimorfismo.test;
+
+import academy.devdojo.maratonajava.javacore.Npolimorfismo.domain.Computador;
+import academy.devdojo.maratonajava.javacore.Npolimorfismo.domain.Produto;
+import academy.devdojo.maratonajava.javacore.Npolimorfismo.domain.Tomate;
+
+public class ProdutoTest02 {
+    public static void main(String[] args) {
+        Produto produto1 = new Computador("Ryzen 9", 3000);
+        System.out.println(produto1.getNome());
+        System.out.println(produto1.getValor());
+        System.out.println(produto1.calcularImposto());
+
+        System.out.println("-------------------");
+
+        Produto produto2 = new Tomate("Saladete", 8);
+        System.out.println(produto2.getNome());
+        System.out.println(produto2.getValor());
+        System.out.println(produto2.calcularImposto());
+    }
+}
