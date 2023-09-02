3f863874ec63bccf3d1e775747a919c3e5fedaad
danielpereira91 <daniel_t.pereira@hotmail.com>
2023-09-02 01:47:07 -0300

    Aula 92 - Polimorfismo pt 03 - Parâmetros polimórficos

--- src/academy/devdojo/maratonajava/javacore/Npolimorfismo/test/ProdutoTest01.java
+++ src/academy/devdojo/maratonajava/javacore/Npolimorfismo/test/ProdutoTest01.java
@@ -1,6 +1,7 @@
 package academy.devdojo.maratonajava.javacore.Npolimorfismo.test;
 
 import academy.devdojo.maratonajava.javacore.Npolimorfismo.domain.Computador;
+import academy.devdojo.maratonajava.javacore.Npolimorfismo.domain.Televisao;
 import academy.devdojo.maratonajava.javacore.Npolimorfismo.domain.Tomate;
 import academy.devdojo.maratonajava.javacore.Npolimorfismo.servico.CalculadoraImposto;
 
@@ -8,8 +9,11 @@ public class ProdutoTest01 {
     public static void main(String[] args) {
         Computador computador = new Computador("NUC-10i7", 11000);
         Tomate tomate = new Tomate("Rasteiro", 10);
-        CalculadoraImposto.calcularImpostoComputador(computador);
+        Televisao tv = new Televisao("Samsung 50\"", 5000);
+        CalculadoraImposto.calcularImposto(computador);
         System.out.println("---------------");
-        CalculadoraImposto.calcularImpostoTomate(tomate);
+        CalculadoraImposto.calcularImposto(tomate);
+        System.out.println("---------------");
+        CalculadoraImposto.calcularImposto(tv);
     }
 }
